f3eb209e3ae0a45e39fc7de0eac1826e5fc91506
zar <zar@qq.com>
2022-05-22 22:39:03 +0800

    增加了一个Demo类，修改了HelloWeb类中的内容

---
+++ git_new_001/src/main/java/com/bjpowernode/Demo.java
@@ -0,0 +1,7 @@
+package com.bjpowernode;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("demo----demo----demo!");
+    }
+}
--- git_new_002web/src/main/java/com/bjpowernode/HelloWeb.java
+++ git_new_002web/src/main/java/com/bjpowernode/HelloWeb.java
@@ -3,5 +3,6 @@ package com.bjpowernode;
 public class HelloWeb {
     public static void main(String[] args) {
         System.out.println("java web new ..................");
+        System.out.println("我是修改的内容...........");
     }
 }
