3611675c4ce71914841d2b944f12edd0065d5709
zar <zar@qq.com>
2022-05-22 18:14:51 +0800

    提交整个项目

---
+++ git_new_001/src/main/java/com/bjpowernode/HelloNew.java
@@ -0,0 +1,7 @@
+package com.bjpowernode;
+
+public class HelloNew {
+    public static void main(String[] args) {
+        System.out.println("hello world new...............");
+    }
+}
---
+++ git_new_002web/src/main/java/com/bjpowernode/HelloWeb.java
@@ -0,0 +1,7 @@
+package com.bjpowernode;
+
+public class HelloWeb {
+    public static void main(String[] args) {
+        System.out.println("java web new ..................");
+    }
+}
