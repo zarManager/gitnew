32823e981fe88f0aa82a09bdda891f4ed37ce562
zar <zar@qq.com>
2022-05-23 15:22:01 +0800

    项目经理再次更新的内容

--- git_new_001/src/main/java/com/bjpowernode/HelloNew.java
+++ git_new_001/src/main/java/com/bjpowernode/HelloNew.java
@@ -2,6 +2,8 @@ package com.bjpowernode;
 
 public class HelloNew {
     public static void main(String[] args) {
+
         System.out.println("hello world new...............");
+        System.out.println("这是项目经理更新后的内容，为了演示程序员的拉取更新....");
     }
 }
